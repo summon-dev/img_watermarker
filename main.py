b3bec1dfeca5f108d25a7eb5566e2ca5e12670ae
samondev <s_monfared1@yahoo.com>
2023-09-25 17:07:53 +0300

    Path label update, still output folder "out"

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def open_file():
     # save the image path in a raw string
     file_path = r"{}".format(file_name)
     # print(file_path.rsplit('/', 1)[0])
-    print_out()
+    # print_out()
+    blank_label.config(text=file_path)
     return file_path
 
 
@@ -34,14 +35,11 @@ def watermark_text(file_path, text):
 
     # Correcting image orientation
     image = ImageOps.exif_transpose(image)
-    position = (image.size[0] - 600, image.size[1] - 300)
-    # print(image.size[0])
-    # print(image.size[1])
+    position = (image.size[0] - 800, image.size[1] - 300)
 
     # Blank image
     img_txt = Image.new("RGBA", image.size, (255, 255, 255, 0))
-    # print(img_txt.size[0])
-    # print(img_txt.size[1])
+
     # Get a font
     fnt = ImageFont.truetype("arial.ttf", 150)
 
@@ -57,15 +55,14 @@ def watermark_text(file_path, text):
 
     # compose text image and original image to make it watermarked
     watermarked_img = Image.alpha_composite(image, img_txt)
-    # print(watermarked_img.size[0])
-    # print(watermarked_img.size[1])
+
     # watermarked_img.show()
     out_file = file_path.rsplit(
-        '/', 1)[0] + '/out/' + file_path.rsplit('/', 1)[1]
-    print(out_file)
+        '/', 1)[0] + '/out/' + file_path.rsplit('/', 1)[1].rsplit('.')[0] + '.jpg'
+    # print(out_file)
 
     watermarked_img = watermarked_img.convert('RGB')
-    watermarked_img.save(out_file)
+    watermarked_img.save(out_file, 'JPEG')
 
     # watermarked_img.show()
 
@@ -76,32 +73,39 @@ def watermark_text(file_path, text):
 
 window = Tk("Image WaterMarker")
 window.title("Image Watermarker")
-window.minsize(width=400, height=100)
+window.geometry("700x500")
+# window.minsize(width=400, height=100)
 window.config(padx=20, pady=20)
 
+# Open file section
 my_label = Label(text="Select Source File:", font=("Arial", 10))
 my_label.config(padx=5, pady=5)
 my_label.grid(column=0, row=0)
+# Open file button with fuction to get the file name and path
 open_button = MyButton(
     text="Open File", command=lambda: open_button.clicked(open_file))
 open_button.config(padx=5, pady=5)
 open_button.grid(column=1, row=0, sticky=W)
+
+# Watermark text Section
 label_wtmk = Label(text="Enter Watermark Text:", font=("Arial", 10))
 label_wtmk.grid(column=2, row=0, sticky=E)
 label_wtmk.config(padx=5, pady=5)
-blank_label = Label(text="              ", font=("Arial", 10))
+blank_label = Label(text=open_button.data, font=("Arial", 10))
 blank_label.grid(column=0, row=1, columnspan=4)
 input = Entry()
 input.grid(column=3, row=0)
 
-if open_button.data != None:
-    print("the open button data -> " + open_button.data)
+# Frame for picture display
 
+
+# The button to apply text on the image
 print_button = MyButton(window, text="Create", command=lambda: print_button.clicked(
     watermark_text, open_button.data, input.get()))
-# print(open_button.data)
 print_button.grid(column=0, row=2, columnspan=4)
 print_button.config(padx=50, pady=5)
+
+print(open_button.data)
 
 
 window.mainloop()
